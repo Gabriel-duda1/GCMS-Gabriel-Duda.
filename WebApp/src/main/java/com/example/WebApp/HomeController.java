70a931785d2c32f42ae0ad37c7ca15c8a6ee011a
“Gabriel-duda1” <gabrieldudapro@gmail.com>
2025-12-11 11:15:06 -0300

    Adicionei teste

--- WebApp/src/main/java/com/example/WebApp/HomeController.java
+++ WebApp/src/main/java/com/example/WebApp/HomeController.java
@@ -1,22 +1,27 @@
- package com.example.WebApp;
+package com.example.WebApp;
 
- import org.springframework.stereotype.Controller;
- import org.springframework.web.bind.annotation.RequestMapping;
- import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-	
-@RequestMapping("/")
- public @ResponseBody String greeting() {
-	
-		return "Hello, World!";
-		}
 
-@RequestMapping("/Biel")
- public @ResponseBody String greetingBiel() {
+	@RequestMapping("/")
+	public @ResponseBody String greeting() {
+
+		return "Pratica 3, Biel!";
+	}
+
+	@RequestMapping("/Biel")
+	public @ResponseBody String greetingBiel() {
 		return "Hello, Biel!";
 	}
 
-}
+	@Test
+	public void testEndpointTeste() {
+		String expected = "Endpoint de teste!";
+		assertEquals(expected, controller.teste());
+	}
 
+}
